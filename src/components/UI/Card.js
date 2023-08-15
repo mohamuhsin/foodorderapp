f1f3e4125b19891211aba4c2c26ee1365f33656f
mohamuhsin <muhsinohoho@outlook.com>
2023-08-15 14:35:35 +0300

    created card component and styled it

---
+++ src/components/UI/Card.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import classes from "./Card.module.css";
+
+const Card = (props) => {
+  return (
+    <Fragment>
+      <div className={`${classes.card} ${props.className}`}>
+        {props.children}
+      </div>
+    </Fragment>
+  );
+};
+
+export default Card;
