462ebb24af779bb5b772dc1910ea493d1f032273
mohamuhsin <muhsinohoho@outlook.com>
2023-08-15 15:17:13 +0300

    created reusable input component and used it in mealitemform component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,20 @@
 import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
   return (
     <form className={classes.form}>
-      <input type="text" />
+      <Input
+        label="amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          mac: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
     </form>
   );
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,4 +1,11 @@
-const Input = () => {
-  return;
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  );
 };
 export default Input;
